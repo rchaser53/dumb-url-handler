cad372fc225f79c5d7c2ff315e1a700ca58f32ff
rchaser53 <tayoshizawa29@gmail.com>
2018-01-28 11:21:08 +0900

    add typescript definition

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,9 @@ export declare const urlHandler: (targetPath: string) => string;
 export declare const hostHandler: (targetHost: string) => string;
 export declare const createUrl: (host: string, paths: string[]) => string;
 export declare const addQueryAttacher: (url: string) => string;
+export declare const parseQuery: (url: string) => {
+    [key: string]: any;
+};
 export declare const addQuery: (url: string, queryObj: {
     [key: string]: any;
 }) => string;
